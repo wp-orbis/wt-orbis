7940c250524e19f2f3f344a2fe70b67543292098
Remco Tolsma <info@remcotolsma.nl>
2012-12-21 15:22:09 +0100

    WordPress Coding Standards.

--- template-timesheets.php
+++ template-timesheets.php
@@ -8,34 +8,36 @@ get_header(); ?>
 <?php
 
 // Get week range
-function get_week_range($date) {
-	$ts = strtotime($date);
-	$start = strtotime('sunday this week -1 week', $ts);
-	$end = strtotime('sunday this week', $ts);
-	return array(date('Y-m-d', $start), date('Y-m-d', $end));
+function get_week_range( $date ) {
+	$ts    = strtotime( $date );
+	$start = strtotime( 'sunday this week -1 week', $ts );
+	$end   = strtotime( 'sunday this week', $ts );
+
+	return array( date( 'Y-m-d', $start ), date( 'Y-m-d', $end ) );
 }
-$dates = get_week_range(date('Y-m-d'));
+
+$dates = get_week_range( date('Y-m-d') );
 
 // Handle query variables
-$user = filter_input(INPUT_GET, 'user', FILTER_SANITIZE_STRING);
-$start_date = filter_input(INPUT_GET, 'start_date', FILTER_SANITIZE_STRING);
-$end_date = filter_input(INPUT_GET, 'end_date', FILTER_SANITIZE_STRING);
+$user       = filter_input( INPUT_GET, 'user', FILTER_SANITIZE_STRING );
+$start_date = filter_input( INPUT_GET, 'start_date', FILTER_SANITIZE_STRING );
+$end_date   = filter_input( INPUT_GET, 'end_date', FILTER_SANITIZE_STRING );
 
 // Set start date
-if($start_date) {
+if ( $start_date ) {
 	$start_date = $start_date;
 } else {
 	$start_date = $dates[0];
 }
 
 // Set end date
-if($end_date) {
+if ( $end_date ) {
 	$end_date = $end_date;
 } else {
 	$end_date = $dates[1];
 }
 
-if($user) {
+if ( $user ) {
 	$query = "WHERE user_id = " . $_GET['user'] . " AND date >= '$start_date' AND date <= '$end_date' ORDER BY date ASC";
 } else {
 	$query = "WHERE date >= '$start_date'  AND date < '$end_date' ORDER BY date ASC";
@@ -43,27 +45,27 @@ if($user) {
 
 global $wpdb;
 
-$result = $wpdb->get_results( "SELECT orbis_hours_registration.*, orbis_projects.invoicable FROM orbis_hours_registration LEFT JOIN orbis_projects ON(orbis_hours_registration.project_id = orbis_projects.id ) $query");
+$result = $wpdb->get_results( "SELECT orbis_hours_registration.*, orbis_projects.invoicable FROM orbis_hours_registration LEFT JOIN orbis_projects ON(orbis_hours_registration.project_id = orbis_projects.id ) $query" );
 
-$total_seconds = 0;
-$billable_seconds = 0;
+$total_seconds      = 0;
+$billable_seconds   = 0;
 $unbillable_seconds = 0;
 
-foreach($result as $row) {
+foreach ( $result as $row ) {
 	$total_seconds += $row->number_seconds;
 	
-	if($row->invoicable) {
-		$billable_seconds += $row->number_seconds;
+	if ( $row->invoicable ) {
+		$billable_seconds   += $row->number_seconds;
 	} else {
 		$unbillable_seconds += $row->number_seconds;
 	}
 }
 
 $unbillable_hours = $unbillable_seconds / 60 / 60;
-$billable_hours = $billable_seconds / 60 / 60;
-$total_hours = $total_seconds / 60 / 60;
+$billable_hours   = $billable_seconds / 60 / 60;
+$total_hours      = $total_seconds / 60 / 60;
 
-if($total_seconds > 0 ) {
+if ( $total_seconds > 0 ) {
 	$total = $billable_seconds / $total_seconds  * 100;
 } else {
 	$total = 0;
@@ -87,8 +89,8 @@ $amount = $billable_hours * 75;
 	<div class="span6">
 		<form method="get" class="form-inline">
 			View report from
-			<input type="text" name="start_date" class="input-small" placeholder="0000-00-00" value="<?php if($start_date) { echo $start_date; } else { echo '2010-06-01'; }?>"> to
-			<input type="text" name="end_date" class="input-small" placeholder="0000-00-00" value="<?php if($end_date) { echo $end_date; } else { echo '2010-06-10'; }?>">
+			<input type="text" name="start_date" class="input-small" placeholder="0000-00-00" value="<?php if ( $start_date ) { echo $start_date; } else { echo '2010-06-01'; }?>"> to
+			<input type="text" name="end_date" class="input-small" placeholder="0000-00-00" value="<?php if ( $end_date ) { echo $end_date; } else { echo '2010-06-10'; }?>">
 
 			<button type="submit" class="btn">Filter</button>
 		</form>
@@ -98,12 +100,12 @@ $amount = $billable_hours * 75;
 		<form name="userForm" method="get" class="form-inline pull-right">
 			<select name="user" id="userSelect">
 				<option value="">Alle gebruikers</option>
-				<option value="1"<?php if($user == 1) echo ' selected'; ?>>Jelke Boonstra</option>
-				<option value="3"<?php if($user == 3) echo ' selected'; ?>>Leo Oosterloo</option>
-				<option value="4"<?php if($user == 4) echo ' selected'; ?>>Jan Lammert Sijtsema</option>
-				<option value="5"<?php if($user == 5) echo ' selected'; ?>>Karel-Jan Tolsma</option>
-				<option value="6"<?php if($user == 6) echo ' selected'; ?>>Remco Tolsma</option>
-				<option value="24"<?php if($user == 24) echo ' selected'; ?>>Martijn Duker</option>
+				<option value="1"<?php if ( $user == 1 ) echo ' selected'; ?>>Jelke Boonstra</option>
+				<option value="3"<?php if ( $user == 3 ) echo ' selected'; ?>>Leo Oosterloo</option>
+				<option value="4"<?php if ( $user == 4 ) echo ' selected'; ?>>Jan Lammert Sijtsema</option>
+				<option value="5"<?php if ( $user == 5 ) echo ' selected'; ?>>Karel-Jan Tolsma</option>
+				<option value="6"<?php if ( $user == 6 ) echo ' selected'; ?>>Remco Tolsma</option>
+				<option value="24"<?php if ( $user == 24 ) echo ' selected'; ?>>Martijn Duker</option>
 			</select>
 		</form>
 	</div>
@@ -113,10 +115,10 @@ $amount = $billable_hours * 75;
 
 <div class="row">
 	<div class="span12">
-		<h1><?php echo round($total) . '%'; ?> <span style="font-size: 16px; font-weight: normal;">of the hours are billable</span> </h1>
+		<h1><?php echo round( $total ) . '%'; ?> <span style="font-size: 16px; font-weight: normal;">of the hours are billable</span> </h1>
 
 		<div class="progress progress-striped active">
-			<div class="bar" style="width: <?php echo round($total) . '%'; ?>;"></div>
+			<div class="bar" style="width: <?php echo round( $total ) . '%'; ?>;"></div>
 		</div>
 	</div>
 </div>
@@ -155,24 +157,24 @@ $amount = $billable_hours * 75;
 		</tr>
 	</thead>
 	<tbody>
-		<?php $date = 0; foreach( $result as $row ): ?>
-
-		<?php if( $date != $row->date ) : $date = $row->date; $total = 0; ?>
-		
-		<tr>
-			<td colspan="4"><h2><?php echo $row->date; ?></h2></td>
-		</tr>
-		
-		<?php endif; ?>
-		
-		<?php $total += $row->number_seconds; ?>
-
-		<tr>
-			<td><?php echo $row->project_id; ?></td>
-			<td><?php echo $row->description; ?></td>
-			<td><?php echo gmdate( 'H:i', $row->number_seconds ); ?></td>
-			<td><?php echo gmdate( 'H:i', $total ); ?></td>
-		</tr>
+		<?php $date = 0; foreach( $result as $row ) : ?>
+	
+			<?php if( $date != $row->date ) : $date = $row->date; $total = 0; ?>
+			
+				<tr>
+					<td colspan="4"><h2><?php echo $row->date; ?></h2></td>
+				</tr>
+			
+			<?php endif; ?>
+			
+			<?php $total += $row->number_seconds; ?>
+	
+			<tr>
+				<td><?php echo $row->project_id; ?></td>
+				<td><?php echo $row->description; ?></td>
+				<td><?php echo gmdate( 'H:i', $row->number_seconds ); ?></td>
+				<td><?php echo gmdate( 'H:i', $total ); ?></td>
+			</tr>
 
 		<?php endforeach; ?>
 	</tbody>
