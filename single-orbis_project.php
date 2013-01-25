c6ec8f7865802d6a920cfbe4cc407036b42174e9
Remco Tolsma <info@remcotolsma.nl>
2013-01-25 12:10:10 +0100

    Moved the flot function to Orbis plugin.

--- single-orbis_project.php
+++ single-orbis_project.php
@@ -158,15 +158,6 @@
 			echo $row->activity_name . ' - <strong>' . orbis_format_seconds($row->total_seconds) . '</strong><br />';
 		}
 		
-		function orbis_flot( $id, $data, $options ) {
-			printf(
-				'<script type="text/javascript">jQuery.plot("#%s", %s, %s);</script>',
-				$id,
-				json_encode( $data ),
-				json_encode( $options )
-			);
-		}
-		
 		$flot_data = array();
 		
 		foreach ( $result as $row ) {
