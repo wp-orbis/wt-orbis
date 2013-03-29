13f9e4016e5a699cf75fdacd41eb9ff14d2c1594
Remco Tolsma <info@remcotolsma.nl>
2013-03-29 08:09:23 +0100

    Added full width template (by @kjtolsma)

---
+++ template-full-width.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * Template Name: Full width
+ */
+
+get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<div class="page-header">
+		<h1>
+			<?php the_title(); ?>
+		</h1>
+	</div>
+	
+	<?php the_content(); ?>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
