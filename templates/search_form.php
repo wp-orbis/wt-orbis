5a9a7a1861ca673c8866814ee5003bca8260a987
Karel-Jan Tolsma <kj.tolsma@gmail.com>
2013-05-13 16:20:27 +0200

    Advanced Search

--- templates/search_form.php
+++ templates/search_form.php
@@ -3,9 +3,29 @@
 $s = filter_input( INPUT_GET, 's', FILTER_SANITIZE_STRING ); 
 
 ?>
-  
-<form class="well form-search" method="get">
-	<input type="text" class="input-medium search-query" name="s" placeholder="<?php esc_attr_e( 'Search', 'orbis' ); ?>" value="<?php echo esc_attr( $s ); ?>" />
 
-	<button type="submit" class="btn"><?php _e( 'Search', 'orbis' ); ?></button>
-</form>
+<div class="well">
+	<form class="form-search" method="get">
+		<input type="text" class="input-medium search-query" name="s" placeholder="<?php esc_attr_e( 'Search', 'orbis' ); ?>" value="<?php echo esc_attr( $s ); ?>" />
+
+		<button type="submit" class="btn"><?php _e( 'Search', 'orbis' ); ?></button> 
+		<small><a href="#" class="advanced-search-link" data-toggle="collapse" data-target="#advanced-search"><?php _e( 'Advanced Search', 'orbis' ); ?></a></small>
+	</form>
+
+	<form id="advanced-search" class="collapse" method="get">
+		<fieldset>
+			<legend><?php _e( 'Advanced Search', 'orbis' ); ?></legend>
+
+			<label><?php _e( 'Client', 'orbis' ); ?></label>
+			<input type="text" placeholder="<?php _e( 'Search on Client', 'orbis' ); ?>">
+		
+			<label><?php _e( 'Invoice Number', 'orbis' ); ?></label>
+			<input type="text" placeholder="<?php _e( 'Search on Invoice Number', 'orbis' ); ?>">
+
+			<div>
+				<button type="submit" class="btn btn-primary"><?php _e( 'Search', 'orbis' ); ?></button>
+				<button type="button" class="btn" data-toggle="collapse" data-target="#advanced-search"><?php _e( 'Cancel', 'orbis' ); ?></button>
+			</div>
+		</fieldset>
+	</form>
+</div>
