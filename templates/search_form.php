9c621ac322a79dec58d531d0b70c293c15924d5e
Remco Tolsma <info@remcotolsma.nl>
2013-05-31 15:25:48 +0200

    Extended search form with advanced search form.

--- templates/search_form.php
+++ templates/search_form.php
@@ -2,30 +2,51 @@
 
 $s = filter_input( INPUT_GET, 's', FILTER_SANITIZE_STRING ); 
 
+$has_advanced = is_post_type_archive( 'orbis_project' );
+
 ?>
 
 <div class="well">
-	<form class="form-search" method="get">
-		<input type="text" class="input-medium search-query" name="s" placeholder="<?php esc_attr_e( 'Search', 'orbis' ); ?>" value="<?php echo esc_attr( $s ); ?>" />
+	<form method="get">
+		<div class="form-search">
+			<input type="text" class="input-medium search-query" name="s" placeholder="<?php esc_attr_e( 'Search', 'orbis' ); ?>" value="<?php echo esc_attr( $s ); ?>" />
 
-		<button type="submit" class="btn"><?php _e( 'Search', 'orbis' ); ?></button> 
-		<small><a href="#" class="advanced-search-link" data-toggle="collapse" data-target="#advanced-search"><?php _e( 'Advanced Search', 'orbis' ); ?></a></small>
-	</form>
+			<button type="submit" class="btn"><?php _e( 'Search', 'orbis' ); ?></button> 
+			
+			<?php if ( $has_advanced) : ?>
+			
+				<small><a href="#" class="advanced-search-link" data-toggle="collapse" data-target="#advanced-search"><?php _e( 'Advanced Search', 'orbis' ); ?></a></small>
 
-	<form id="advanced-search" class="collapse" method="get">
-		<fieldset>
-			<legend><?php _e( 'Advanced Search', 'orbis' ); ?></legend>
+			<?php endif; ?>
+		</div>
 
-			<label><?php _e( 'Client', 'orbis' ); ?></label>
-			<input type="text" placeholder="<?php _e( 'Search on Client', 'orbis' ); ?>">
+		<?php if ( $has_advanced ) : ?>
+
+			<?php 
+			
+			$principal      = get_query_var( 'orbis_project_principal' );
+			$invoice_number = get_query_var( 'orbis_project_invoice_number' );
+			$is_advanced    = ! empty( $principal ) || ! empty( $invoice_number );
+
+			?>
+
+			<div id="advanced-search" class="<?php echo $is_advanced ? 'in' : 'collapse'; ?>">
+				<fieldset>
+					<legend><?php _e( 'Advanced Search', 'orbis' ); ?></legend>
 		
-			<label><?php _e( 'Invoice Number', 'orbis' ); ?></label>
-			<input type="text" placeholder="<?php _e( 'Search on Invoice Number', 'orbis' ); ?>">
-
-			<div>
-				<button type="submit" class="btn btn-primary"><?php _e( 'Search', 'orbis' ); ?></button>
-				<button type="button" class="btn" data-toggle="collapse" data-target="#advanced-search"><?php _e( 'Cancel', 'orbis' ); ?></button>
+					<label for="orbis_project_principal"><?php _e( 'Client', 'orbis' ); ?></label>
+					<input id="orbis_project_principal" name="orbis_project_principal" value="<?php echo esc_attr( $principal ); ?>" type="text" placeholder="<?php _e( 'Search on Client', 'orbis' ); ?>">
+				
+					<label for="orbis_project_invoice_number"><?php _e( 'Invoice Number', 'orbis' ); ?></label>
+					<input id="orbis_project_invoice_number" name="orbis_project_invoice_number" value="<?php echo esc_attr( $invoice_number ); ?>" type="text" placeholder="<?php _e( 'Search on Invoice Number', 'orbis' ); ?>">
+		
+					<div>
+						<button type="submit" class="btn btn-primary"><?php _e( 'Search', 'orbis' ); ?></button>
+						<button type="button" class="btn" data-toggle="collapse" data-target="#advanced-search"><?php _e( 'Cancel', 'orbis' ); ?></button>
+					</div>
+				</fieldset>
 			</div>
-		</fieldset>
+		
+		<?php endif; ?>
 	</form>
 </div>
