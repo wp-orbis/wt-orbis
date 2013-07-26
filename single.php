8bba992bc1a02a6dc90ee18d0075d22b209c53a4
Remco Tolsma <info@remcotolsma.nl>
2013-07-26 11:47:11 +0200

    Grouped post an div element with post ID and classes.

--- single.php
+++ single.php
@@ -2,63 +2,65 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<div class="page-header">
-		<h1>
-			<?php the_title(); ?>
-			
-			<?php if ( is_singular( 'orbis_company' ) ) : ?>
-			
-				<small><?php _e( 'Company Details', 'orbis' ); ?></small>
-			
-			<?php endif; ?>
-		</h1>
-	</div>
-	
-	<div class="row">
-		<div class="span8">	
-			<?php do_action( 'orbis_before_main_content' ); ?>
+	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<div class="page-header">
+			<h1>
+				<?php the_title(); ?>
 
-			<div class="panel">
-				<header>
-					<h3><?php _e( 'Description', 'orbis' ); ?></h3>
-				</header>
-				
-				<div class="content">
-					<?php the_content(); ?>
-				</div>
-			</div>
-	
-			<?php do_action( 'orbis_after_main_content' ); ?>
-	
-			<?php comments_template( '', true ); ?>
+				<?php if ( is_singular( 'orbis_company' ) ) : ?>
+
+					<small><?php _e( 'Company Details', 'orbis' ); ?></small>
+
+				<?php endif; ?>
+			</h1>
 		</div>
-	
-		<div class="span4">
-			<?php do_action( 'orbis_before_side_content' ); ?>
 
-			<div class="panel">
-				<header>
-					<h3><?php _e( 'Additional Information', 'orbis' ); ?></h3>
-				</header>
-	
-				<div class="content">
-					<dl>
-						<dt><?php _e( 'ID', 'orbis' ); ?></dt>
-						<dd><?php echo get_the_id(); ?></dd>
+		<div class="row">
+			<div class="span8">
+				<?php do_action( 'orbis_before_main_content' ); ?>
 
-						<dt><?php _e( 'Posted on', 'orbis' ); ?></dt>
-						<dd><?php echo get_the_date(); ?></dd>
+				<div class="panel">
+					<header>
+						<h3><?php _e( 'Description', 'orbis' ); ?></h3>
+					</header>
 
-						<dt><?php _e( 'Posted by', 'orbis' ); ?></dt>
-						<dd><?php echo get_the_author(); ?></dd>
-
-						<dt><?php _e( 'Actions', 'orbis' ); ?></dt>
-						<dd><?php edit_post_link( __( 'Edit', 'orbis' ) ); ?></dd>
-					</dl>
+					<div class="content">
+						<?php the_content(); ?>
+					</div>
 				</div>
+
+				<?php do_action( 'orbis_after_main_content' ); ?>
+
+				<?php comments_template( '', true ); ?>
 			</div>
-			
-			<?php do_action( 'orbis_after_side_content' ); ?>
+
+			<div class="span4">
+				<?php do_action( 'orbis_before_side_content' ); ?>
+
+				<div class="panel">
+					<header>
+						<h3><?php _e( 'Additional Information', 'orbis' ); ?></h3>
+					</header>
+
+					<div class="content">
+						<dl>
+							<dt><?php _e( 'ID', 'orbis' ); ?></dt>
+							<dd><?php echo get_the_id(); ?></dd>
+
+							<dt><?php _e( 'Posted on', 'orbis' ); ?></dt>
+							<dd><?php echo get_the_date(); ?></dd>
+
+							<dt><?php _e( 'Posted by', 'orbis' ); ?></dt>
+							<dd><?php echo get_the_author(); ?></dd>
+
+							<dt><?php _e( 'Actions', 'orbis' ); ?></dt>
+							<dd><?php edit_post_link( __( 'Edit', 'orbis' ) ); ?></dd>
+						</dl>
+					</div>
+				</div>
+
+				<?php do_action( 'orbis_after_side_content' ); ?>
+			</div>
 		</div>
 	</div>
 
