dfa06c477ca11263b74935a0eb9d6e7eefaed3ec
Remco Tolsma <info@remcotolsma.nl>
2013-07-31 11:47:51 +0200

    Removed Bootstrap CSS declaration from header, we now use wp_enqueue_style() for this.

--- header.php
+++ header.php
@@ -4,16 +4,16 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
+
 		<title>
 			<?php
 
 			global $page, $paged;
-		
+
 			wp_title( '|', true, 'right' );
-		
+
 			bloginfo( 'name' );
-		
+
 			$site_description = get_bloginfo( 'description', 'display' );
 
 			if ( $site_description && ( is_home() || is_front_page() ) ) {
@@ -23,15 +23,13 @@
 			if ( $paged >= 2 || $page >= 2 ) {
 				echo ' | ' . sprintf( __( 'Page %s', 'orbis' ), max( $paged, $page ) );
 			}
-		
+
 			?>
 		</title>
 
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/bootstrap.css" />
-		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/bootstrap-responsive.css" />
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
 		<!--[if lt IE 9]>
@@ -46,13 +44,13 @@
 		<link rel="apple-touch-icon-precomposed" href="<?php bloginfo('stylesheet_directory'); ?>/icons/apple-touch-icon-57-precomposed.png">
 
 		<?php
-	
+
 		if( is_singular() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
-	
+
 		wp_head();
-	
+
 		?>
 	</head>
 
@@ -69,19 +67,19 @@
 					<a class="brand" href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 
 					<div class="nav-collapse">
-						<?php 
+						<?php
 
 						$args = array(
 							'container' => false,
 							'theme_location' => 'primary',
-							'menu_class' => 'nav',
+							'menu_class' => 'nav navbar-nav',
 							'walker' => new Bootstrap_Walker_Nav_Menu()
 						);
 
 						wp_nav_menu($args);
 
 						$s = filter_input( INPUT_GET, 's', FILTER_SANITIZE_STRING );
-						
+
 						?>
 
 						<form method="get" class="navbar-search pull-left" action="<?php echo esc_url( home_url( '/' ) ); ?>">
@@ -89,19 +87,19 @@
                     	</form>
 
 						<?php if ( is_user_logged_in() ) : ?>
-				
-							<?php 
-					
+
+							<?php
+
 							global $current_user;
-					
+
 							get_currentuserinfo();
-					
+
 							?>
-	
+
 							<ul class="nav pull-right">
 								<li class="dropdown">
 									<a data-toggle="dropdown" class="dropdown-toggle" href="#"><?php echo get_avatar( $current_user->ID, 20 ); ?> <?php echo $current_user->user_login; ?> <b class="caret"></b></a>
-	
+
 									<ul class="dropdown-menu">
 										<li><a href="http://orbiswp.com/help/"><i class="icon-question-sign"></i> <?php _e( 'Help', 'orbis' ); ?></a></li>
 										<li><a href="<?php echo admin_url( 'profile.php' ); ?>"><i class="icon-user"></i> <?php _e( 'Edit profile', 'orbis' ); ?></a></li>
@@ -116,5 +114,5 @@
 				</div>
 			</div>
 		</div>
-		
+
 		<div class="container">
