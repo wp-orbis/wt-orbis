508d4ce5e6502dd4bdb2dd87743177e2950fe201
Karel-Jan Tolsma <kj.tolsma@gmail.com>
2013-12-12 16:01:24 +0100

    Favicon

--- templates/person_companies.php
+++ templates/person_companies.php
@@ -17,15 +17,13 @@
 			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
 
 				<li>
-					<?php if ( get_post_meta( $post->ID, '_orbis_company_website', true ) ) : ?>
+					<?php if ( get_post_meta( get_the_ID(), '_orbis_company_website', true ) ) : ?>
 				
-						<?php $favicon_url = add_query_arg( 'domain', get_post_meta( $post->ID, '_orbis_company_website', true ), 'https://plus.google.com/_/favicon' ); ?>
-				
-						<img src="<?php echo esc_attr( $favicon_url ); ?>" alt="" />
+						<?php $favicon_url = add_query_arg( 'domain', get_post_meta( get_the_ID(), '_orbis_company_website', true ), 'https://plus.google.com/_/favicon' ); ?>
 
 					<?php endif; ?>
 
-					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					<a href="<?php the_permalink(); ?>"><?php if ( isset( $favicon_url ) ) : ?><img src="<?php echo esc_attr( $favicon_url ); ?>" alt="" /> <?php endif; ?><?php the_title(); ?></a>
 				</li>
 
 			<?php endwhile; ?>
