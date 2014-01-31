e25df23225b093c90a28c953f95613bd53bb2f9c
Boonstra <stefanboonstra92@gmail.com>
2014-01-31 14:08:58 +0100

    Added an orbis_deal archive

---
+++ archive-orbis_deal.php
@@ -0,0 +1,91 @@
+<?php get_header(); ?>
+
+<div class="page-header clearfix">
+	<h1 class="pull-left">
+		<?php post_type_archive_title(); ?>
+
+		<small>
+			<?php
+			
+			printf( __( 'Overview of %1$s', 'orbis' ),
+				strtolower( post_type_archive_title( '', false ) )
+			);
+		
+			?>
+		</small>
+	</h1>
+
+	<a class="btn btn-primary pull-right" href="<?php echo orbis_get_url_post_new(); ?>">
+		<span class="glyphicon glyphicon-plus"></span> <?php _e( 'Add deal', 'orbis' ); ?>
+	</a>
+</div>
+
+<div class="panel">
+	<header>
+		<h3><?php _e( 'Overview', 'orbis' ); ?></h3>
+	</header>
+
+	<?php get_template_part( 'templates/search_form' ); ?>
+	
+	<?php if ( have_posts() ) : ?>
+	
+		<table class="table table-striped table-bordered table-condense table-hover">
+			<thead>
+				<tr>
+					<th><?php _e( 'Title'  , 'orbis' ); ?></th>
+					<th><?php _e( 'Company', 'orbis' ); ?></th>
+					<th><?php _e( 'Price'  , 'orbis' ); ?></th>
+					<th><?php _e( 'Status' , 'orbis' ); ?></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php while ( have_posts() ) : the_post(); ?>
+	
+					<tr id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<td>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+
+							<?php if ( get_comments_number() != 0  ) : ?>
+							
+								<div class="comments-number">
+									<span class="glyphicon glyphicon-comment"></span>
+									<?php comments_number( '0', '1', '%' ); ?>
+								</div>
+							
+							<?php endif; ?>
+						</td>
+						<td>
+							<?php orbis_deal_the_company_name(); ?>
+						</td>
+						<td>
+							<?php orbis_deal_the_price(); ?>
+						</td>
+						<td>
+							<div class="actions">
+								<?php orbis_deal_the_status(); ?>
+							
+								<div class="nubbin">
+									<?php orbis_edit_post_link(); ?>
+								</div>
+							</div>
+						</td>
+					</tr>
+	
+				<?php endwhile; ?>
+			</tbody>
+		</table>
+
+	<?php else : ?>
+
+		<div class="content">
+			<p class="alt">
+				<?php _e( 'No results found.', 'orbis' ); ?>
+			</p>
+		</div>
+
+	<?php endif; ?>
+</div>
+
+<?php orbis_content_nav(); ?>
+
+<?php get_footer(); ?>
