50590da47d21ff7b9cfe23c18b3ffc33e8f7d707
Remco Tolsma <info@remcotolsma.nl>
2014-03-05 16:00:06 +0100

    By default display all comment types.

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -80,43 +80,44 @@ function orbis_comment( $comment, $args, $depth ) {
 	$GLOBALS['comment'] = $comment;
 
 	switch ( $comment->comment_type ) :
-		case '' :
-	?>
-	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-		<div id="comment-<?php comment_ID(); ?>" class="comment-content">
-		<div class="comment-author vcard">
-			<?php echo get_avatar( $comment, 60 ); ?>
-
-			<?php printf( __( '%s <span class="says">says:</span>', 'orbis' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
-		</div>
-
-		<?php if ( $comment->comment_approved == '0' ) : ?>
-
-		<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'orbis' ); ?></em><br />
-
-		<?php endif; ?>
-
-		<div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
-			<?php printf( __( '%1$s at %2$s', 'orbis' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'orbis' ), ' '); ?>
-		</div>
-
-		<div class="comment-body">
-			<?php comment_text(); ?>
-		</div>
-
-		<div class="reply">
-			<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-		</div>
-	</div>
-
-	<?php
-			break;
 		case 'pingback'  :
 		case 'trackback' :
-	?>
-	<li class="post pingback">
-		<p><?php _e( 'Pingback:', 'orbis' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'orbis' ), ' ' ); ?></p>
-	<?php
+			?>
+			<li class="post pingback">
+				<p><?php _e( 'Pingback:', 'orbis' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'orbis' ), ' ' ); ?></p>
+			<?php
+			break;
+
+		default :
+			?>
+			<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+				<div id="comment-<?php comment_ID(); ?>" class="comment-content">
+				<div class="comment-author vcard">
+					<?php echo get_avatar( $comment, 60 ); ?>
+		
+					<?php printf( __( '%s <span class="says">says:</span>', 'orbis' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+				</div>
+		
+				<?php if ( $comment->comment_approved == '0' ) : ?>
+		
+				<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'orbis' ); ?></em><br />
+		
+				<?php endif; ?>
+		
+				<div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
+					<?php printf( __( '%1$s at %2$s', 'orbis' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'orbis' ), ' '); ?>
+				</div>
+		
+				<div class="comment-body">
+					<?php comment_text(); ?>
+				</div>
+		
+				<div class="reply">
+					<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+				</div>
+			</div>
+		
+			<?php
 			break;
 	endswitch;
 }
