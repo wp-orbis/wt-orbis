baa431cca7e0308fc2ad0ab8448cdb75affb8920
Remco Tolsma <info@remcotolsma.nl>
2014-09-04 17:01:37 +0200

    Update my tasks.

--- template-my-tasks.php
+++ template-my-tasks.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 /**
  * Template Name: My Tasks
  */
 
 wp_enqueue_script( 'angular' );
 wp_enqueue_style( 'angular-scp' );
+wp_enqueue_script( 'orbis-tasks-angular' );
 
 get_header(); ?>
 
@@ -20,8 +21,20 @@ get_header(); ?>
 		<div class="content">
 			<?php the_content(); ?>
 
-			<div ng-controller="OrbisTasksCtrl" ng-init="test = 'hoi';">
-				{{test}}
+			<div ng-controller="OrbisTasksCtrl">
+			    <h2>You've got <span class="emphasis">{{getTotalTodos()}}</span> things to do</h2>
+			      <ul>
+			        <li ng-repeat="todo in todos">
+			          <input type="checkbox" ng-model="todo.done"/>
+			          <span class="done-{{todo.done}}">{{todo.text}}</span>
+			        </li>
+			      </ul>
+			      <form>
+			        <input class="add-input" placeholder="I need to..." type="text" ng-model="formTodoText" ng-model-instant />
+			        <button class="add-btn" ng-click="addTodo()"><h2>Add</h2></button>
+			      </form>
+
+			      <button class="clear-btn" ng-click="clearCompleted()">Clear completed</button>
 			</div>
 		</div>
 	</div>
