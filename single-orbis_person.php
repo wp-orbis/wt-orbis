284b18c1065f5220abea6d2e6184e0d615ca6da5
Remco Tolsma <info@remcotolsma.nl>
2014-11-07 11:23:50 +0100

    Added support for Twitter widget, we use new option from the Orbis Twitter plugin.

--- single-orbis_person.php
+++ single-orbis_person.php
@@ -121,9 +121,16 @@
 		</div>
 
 		<div class="col-md-4">
-			<?php if ( get_post_meta( $post->ID, '_orbis_person_twitter', true ) ) : ?>
+			<?php if ( get_option( 'orbis_twitter_widget_id' ) && get_post_meta( $post->ID, '_orbis_person_twitter', true ) ) : ?>
 
-				<?php $username = get_post_meta( $post->ID, '_orbis_person_twitter', true ); ?>
+				<?php
+
+				$twitter_widget_id = get_option( 'orbis_twitter_widget_id' );
+				$twitter_username  = get_post_meta( $post->ID, '_orbis_person_twitter', true );
+				$twitter_url       = sprintf( 'https://twitter.com/%s', $username );
+				$twitter_text      = sprintf( __( 'Tweets from @%s', 'orbis' ), $twitter_username );
+
+				?>
 
 				<div class="panel">
 					<header>
@@ -140,7 +147,8 @@
 
 					<div class="content">
 						<p class="alt">
-							<?php _e( 'No tweets available.', 'orbis' ); ?>
+							<a class="twitter-timeline" href="<?php echo esc_attr( $twitter_username ); ?>" data-widget-id="<?php echo esc_attr( $twitter_widget_id ); ?>" data-screen-name="<?php echo $username; ?>" height="300"><?php echo esc_html( $twitter_text ); ?></a>
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 						</p>
 					</div>
 				</div>
