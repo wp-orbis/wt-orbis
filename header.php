6f073c66b59b6d11abf3b6576001a5a488cb57c3
Remco Tolsma <info@remcotolsma.nl>
2014-11-10 12:58:04 +0100

    Added icons.

--- header.php
+++ header.php
@@ -10,6 +10,8 @@
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
+		<?php get_template_part( 'templates/head-icons' ); ?>
+
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
---
+++ templates/head-icons.php
@@ -0,0 +1,10 @@
+<link rel="shortcut icon" href="<?php echo esc_attr( get_template_directory_uri() . '/assets/orbis/icons/icon.png' ); ?>" type="image/x-icon" />
+
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo esc_attr( get_template_directory_uri() . '/assets/orbis/icons/apple-touch-icon-114x114-precomposed.png' ); ?>">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo esc_attr( get_template_directory_uri() . '/assets/orbis/icons/apple-touch-icon-72x72-precomposed.png' ); ?>">
+<link rel="apple-touch-icon-precomposed" href="<?php echo esc_attr( get_template_directory_uri() . '/assets/orbis/icons/apple-touch-icon-precomposed.png' ); ?>">
+
+<!--[if lt IE 9]>
+	<link rel="icon" type="image/vnd.microsoft.icon" href="<?php echo esc_attr( get_template_directory_uri() . '/assets/orbis/icons/favicon.ico' ); ?>" />
+	<link rel="SHORTCUT ICON" href="<?php echo esc_attr( get_template_directory_uri() . '/assets/orbis/icons/favicon.ico' ); ?>" />
+<![endif]-->
